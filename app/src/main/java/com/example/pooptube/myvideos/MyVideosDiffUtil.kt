4c2169b3fe299bd71d0dfa55e184b61080802697
leedaeul <ouowinnie@gmail.com>
2023-09-26 20:24:29 +0900

    [FEAT] : API 어뎁터, setData 설정

---
+++ app/src/main/java/com/example/pooptube/myvideos/MyVideosDiffUtil.kt
@@ -0,0 +1,27 @@
+package com.example.pooptube.myvideos
+
+import androidx.recyclerview.widget.DiffUtil
+
+class MyVideosDiffUtil (
+    private val oldList: List<VideosModelList.VideosModel>,
+    private val newList: List<VideosModelList.VideosModel>,
+
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldVideo = oldList[oldItemPosition]
+        val newVideo = oldList[newItemPosition]
+        return oldVideo.snippet.title == newVideo.snippet.title
+    }
+}
