c4ccdd95b8cde9d9ddf1c94e96f2696283d0ebb8
kyunghwa <snoopn303@gmail.com>
2023-09-27 12:51:26 +0900

    [REFACTOR]: 일단 사용안하는 data 주석처리 참고용으로 주석으로 남겨둠!

--- app/src/main/java/com/example/pooptube/home/HomeVideoModel.kt
+++ app/src/main/java/com/example/pooptube/home/HomeVideoModel.kt
@@ -1,5 +1,6 @@
 package com.example.pooptube.home
 
+import com.example.pooptube.myvideos.YoutubeVideoItem
 import java.util.Date
 
 data class HomeVideoResponse(
@@ -16,37 +17,37 @@ data class HomeVideoModel(
     val isFavorite: Boolean
 )
 
-data class YoutubeVideoItem(
-    val snippet: Snippet,
-    // Search에서는 Statistics 지원x , video에서는 있
-    val statistics: Statistics?
-)
-
-data class Statistics(
-    val viewCount: String,
-    // 필요하면 다른 통계 정보도 추가
-)
-
-data class Snippet(
-    val title: String,
-    val publishedAt: Date,
-    val description: String,
-    val thumbnails: Thumbnails,
-    val channelTitle: String,
-    val channelId: String
-)
-
-data class Thumbnails(
-    val default: ThumbnailInfo,
-    val medium: ThumbnailInfo,
-    val high: ThumbnailInfo
-)
-
-data class ThumbnailInfo(
-    val url: String,
-    val width: Int,
-    val height: Int
-)
+//data class YoutubeVideoItem(
+//    val snippet: Snippet,
+//    // Search에서는 Statistics 지원x , video에서는 있
+//    val statistics: Statistics?
+//)
+//
+//data class Statistics(
+//    val viewCount: String,
+//    // 필요하면 다른 통계 정보도 추가
+//)
+//
+//data class Snippet(
+//    val title: String,
+//    val publishedAt: Date,
+//    val description: String,
+//    val thumbnails: Thumbnails,
+//    val channelTitle: String,
+//    val channelId: String
+//)
+//
+//data class Thumbnails(
+//    val default: ThumbnailInfo,
+//    val medium: ThumbnailInfo,
+//    val high: ThumbnailInfo
+//)
+//
+//data class ThumbnailInfo(
+//    val url: String,
+//    val width: Int,
+//    val height: Int
+//)
 
 //영상의 제목, 설명, 썸네일, 조회수 등)를 받아올 것인지 지정할 수 있습니다. 이를 지정하는 것이 part 파라미터
 //snippet는 영상의 기본 정보(영상 제목, 설명, 썸네일, 채널 정보 등)를 가져오기 위한 파라미터 값
