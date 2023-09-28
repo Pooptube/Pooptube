16a4cd54e565edea96bbee1e3594720f3b969920
ars-yeon <zosel7301@gmail.com>
2023-09-28 11:06:02 +0900

    [FIX] : 🔧(VideoDetailModel): 일부 수정

--- app/src/main/java/com/example/pooptube/videodetail/VideoDetailModel.kt
+++ app/src/main/java/com/example/pooptube/videodetail/VideoDetailModel.kt
@@ -5,10 +5,11 @@ import java.util.Date
 data class VideoDetailModel (
     val thumbnail: String,
     val title: String,
-//    val channelProfile: String, // api에 데이터가 안보임...다시 찾아 볼 예정
+    val channelProfile: String,
     val channelId: String,
     val description: String,
     val dateTime: Date,
-    val isFavorite: Boolean,
-    val viewCount: String
+    val viewCount: String,
+    val videoId: String,
+    var isFavorite: Boolean
 )
