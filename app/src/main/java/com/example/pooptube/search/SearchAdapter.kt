00ce5a9cf1ea0835b43d141d55ea00c2c0cacf9f
leedaeul <ouowinnie@gmail.com>
2023-10-05 00:17:48 +0900

    [FEAT] : SearchAdapter 생성후 분리 작업

--- app/src/main/java/com/example/pooptube/search/SearchAdapter.kt
+++ app/src/main/java/com/example/pooptube/search/SearchAdapter.kt
@@ -1,47 +1,48 @@
-package com.example.pooptube.myvideos
+package com.example.pooptube.search
 
-import android.graphics.Rect
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.pooptube.databinding.MyVideosItemBinding
+import com.example.pooptube.myvideos.MyVideosAdapter
+import com.example.pooptube.myvideos.MyVideosDiffUtil
+import com.example.pooptube.myvideos.YoutubeVideoItem
 import com.example.pooptube.videodetail.VideoDetailModel
 
-class MyVideosAdapter : RecyclerView.Adapter<MyVideosAdapter.VideoHolder>() {
+class SearchAdapter : RecyclerView.Adapter<SearchAdapter.VideoHolder>() {
 
     interface OnItemClickListener {
         fun onItemClick(position: Int)
     }
 
     private var listener: OnItemClickListener? = null
-    private var videoDetailModels = emptyList<VideoDetailModel>()
 
     fun setOnItemClickListener(listener: OnItemClickListener) {
         this.listener = listener
     }
 
+    private var oldItems = emptyList<YoutubeVideoItem>()
+
     class VideoHolder(itemView: MyVideosItemBinding) : RecyclerView.ViewHolder(itemView.root) {
         private val binding = itemView
 
-        fun setData(data: VideoDetailModel) {
-            binding.videoTitle.text = data.title
+        fun setData(data: YoutubeVideoItem){
+            binding.videoTitle.text =  data.snippet.title
             Glide.with(binding.root)
-                .load(data.thumbnail)
+                .load(data.snippet.thumbnails.high.url)
                 .into(binding.videoThumbnail)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVideosAdapter.VideoHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchAdapter.VideoHolder {
         val view = MyVideosItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return VideoHolder(view)
     }
 
     override fun onBindViewHolder(holder: VideoHolder, position: Int) {
-        val videoDetailModel = videoDetailModels[position]
-        holder.setData(videoDetailModel)
+        (holder as VideoHolder).setData(oldItems[position])
 
         holder.itemView.setOnClickListener {
             listener?.onItemClick(position)
@@ -49,11 +50,13 @@ class MyVideosAdapter : RecyclerView.Adapter<MyVideosAdapter.VideoHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return videoDetailModels.size
+        return oldItems.size
     }
 
-    fun setData(newList: List<VideoDetailModel>) {
-        videoDetailModels = newList
-        notifyDataSetChanged()
+    fun setData(newList: List<YoutubeVideoItem>) {
+        val videoDiff = MyVideosDiffUtil(oldItems, newList)
+        val diff = DiffUtil.calculateDiff(videoDiff)
+        oldItems = newList
+        diff.dispatchUpdatesTo(this)
     }
 }
